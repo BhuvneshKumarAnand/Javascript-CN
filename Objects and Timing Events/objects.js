aa23add1d4d349acc0e9c2bfc89011e59715892e
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-11-02 00:33:21 +0530

    Objects in JS

---
+++ Objects and Timing Events/objects.js
@@ -0,0 +1,29 @@
+// Objects in JS 
+// It has key value pairs
+// Javascript object is collection of properties
+
+/*true about JavaScript objects
+they are mutable
+they are created with curly brackets i.e. { }
+they are collection of key value pair(s)*/
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Empty Object
+
+var obj1 = {};
+
+// Object with key-value pair
+
+var student = {
+    Name: "Bharat",
+    RollNo: 17,
+    marks: 90
+};
+
+console.log(typeof student);
+console.log(student.Name);
+console.log(student);
+
+student.sem = 4;
+
+console.log(student);
