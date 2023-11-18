b7dacca710e8b696f8f6d63e9c131c684db879bf
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-11-19 03:20:52 +0530

    revisiting older topics

--- Revisiting Older Concept/script.js
+++ Revisiting Older Concept/script.js
@@ -1,10 +1,28 @@
-var var1 = 10;
+// var var1 = 10;
+
+// function outer() {
+//     var b = 40;
+//     console.log(b);
+// }
+
+// console.log(var1);
+// console.log(window.var1);
+// console.log(window.outer());
+
+var a = 10;
 
 function outer() {
+    var a = 20;
     var b = 40;
-    console.log(b);
+
+    function inner() {
+        var a = 40;
+
+        console.log("Inner", a, b);
+    }
+    inner();
+    console.log("Outer", a, b);
 }
 
-console.log(var1);
-console.log(window.var1);
-console.log(window.outer());
+outer();
+console.log("Global", a);
