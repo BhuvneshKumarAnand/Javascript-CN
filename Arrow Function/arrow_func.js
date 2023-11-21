834b2164fc0e098cc4402112c7558ae226a2482a
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-11-22 04:51:58 +0530

    Arrow Function

---
+++ Arrow Function/arrow_func.js
@@ -0,0 +1,21 @@
+// Normal Function
+
+// var multiply = function(x, y) {
+//     return x * y;
+// }
+
+// Arrow Function
+
+// Method 1
+
+// var multiply = (x, y) => x * y;
+
+
+// Method 2
+
+var multiply = (x, y) => { return x * y };
+
+var double = (x) => 2 * x;
+
+console.log(multiply(10, 3));
+console.log(double(10));
