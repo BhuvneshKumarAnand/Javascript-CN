159786981cd404c8f6b74585048cd87532dfa487
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-12-05 16:29:29 +0530

    find and findIndex function in JS

---
+++ Functions/find.js
@@ -0,0 +1,9 @@
+// Find function and find Index function
+
+const arr = [21, 32, 2, 14, 3, 5, 24, 25, 7, 8, 9];
+
+const find1 = arr.find((num) => num == 14);
+const find2 = arr.findIndex((num) => num == 14);
+
+console.log(find1);
+console.log(find2);
