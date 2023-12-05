7bec7680e99ace0a040664d80a3f2b0f050c868c
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-12-05 15:59:20 +0530

    map

--- Functions/map.js
+++ Functions/map.js
@@ -2,6 +2,9 @@
 
 const arr = [21, 32, 2, 14, 3, 5, 24, 25, 7, 8, 9];
 
+// Syntax
+// arr.map(function(currentElement, currentIndex, Array){//function body}) 
+
 /*const squareArray = arr.map(function(currentElement) {
     return currentElement * currentElement
 });*/
