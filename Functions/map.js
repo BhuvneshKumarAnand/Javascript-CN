f8736e4b649a43897ebe842f8553c49c918567f3
BhuvneshKumarAnand <bharatanand874@gmail.com>
2023-12-05 15:56:31 +0530

    Map function in JS

---
+++ Functions/map.js
@@ -0,0 +1,15 @@
+//  Map in JS
+
+const arr = [21, 32, 2, 14, 3, 5, 24, 25, 7, 8, 9];
+
+/*const squareArray = arr.map(function(currentElement) {
+    return currentElement * currentElement
+});*/
+
+// Arrow Function in map
+
+const squareArray = arr.map(
+    (num) => num * num // Arrow Function
+);
+
+console.log(squareArray);
